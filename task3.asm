77b3fac47ec6e6f23c4ec83fd8ab608bc7abf981
Daniela_Palyan <palyandaniela@gmail.com>
2025-03-17 17:57:03 +0400

    commit4

--- task3.asm
+++ task3.asm
@@ -2,18 +2,17 @@ extern printf
 extern scanf
 section .data 
 	scanf_format db "%d", 0
-	max_printf_format db "Maximum element is: %d", 10, 0
+	printf_format db "Maximum element is: %d", 10, 0
 section .bss
 	arr resd 10
-	max_elem resd 1
 section .text
 	global main:
 main:
+
 	push rbp
 	mov rbp, rsp
-	
+ 
 	xor rbx, rbx
-
 L1:
 	cmp rbx, 10
 	je L1_END
@@ -27,7 +26,48 @@ L1:
 	inc rbx
 	jmp L1
 	
-L1_End:
+L1_END:
+	xor rax, rax
+        mov eax, dword[arr]
+	mov rbx, 1
+L2:
+	cmp rbx, 10
+	je L2_END
 	
+	cmp eax, [arr + 4 * rbx]
+	jl ASSIGN
+	inc rbx	
+	jmp L2
 	
+ASSIGN:
+	mov eax, dword[arr + 4 * rbx]
+	inc rbx
+	jmp L2
+
+L2_END:
+	mov rdi, printf_format
+	movsx rsi, eax
+	xor rax, rax
+	call printf
+
+	mov rsp, rbp
+	pop rbp
+
+	xor rax, rax
+	ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
