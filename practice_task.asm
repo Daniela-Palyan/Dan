12c6daa92c94c93172832cbecf2fe2c45013972b
Daniela_Palyan <palyandaniela@gmail.com>
2025-03-19 16:30:29 +0400

    gone 0

--- practice_task.asm
+++ practice_task.asm
@@ -13,42 +13,48 @@ main:
 	mov rbp, rsp
 	
 
-	sub rsp, 8
+	sub rsi, 4
 	mov rdi, fmt_a
 	xor rax,rax
 	call printf
+
 	mov rsi, rsp
 	mov rdi, fmt_in
 	call scanf
 
-	sub rsp, 8
+	sub rsi, 4
 	mov rdi, fmt_b
         xor rax,rax
         call printf
+
         mov rsi, rsp
         mov rdi, fmt_in
         call scanf
 
-	pop rbx ;b
-	pop rcx ;a
-	push rcx
+	xor rcx, rcx
+	xor rax, rax
+	mov ecx, dword[rsp]
+	add rsp, 4
+	mov eax, dword[rsp]
+	add rsp, 4
+
+	push rax
 	mov rdx, 1
 
 L:
-	cmp rbx, 0
-	je result
-	
-	imul rdx, rcx
-	dec rbx
+	cmp rcx, 0
+	je result	
+	imul rdx, rax
+	dec rcx
 	jmp L 
 
 result:
-	push rdx
 
 	mov rdi, fmt_out
-	pop rsi
+	mov rsi, rdx
 	xor rax, rax
 	call printf
+
 
 	mov rsp, rbp
 	pop rbp
