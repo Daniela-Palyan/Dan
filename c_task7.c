706967dfc2cd3249d6838532e0b2bb12f3cbe553
Daniela_Palyan <palyandaniela@gmail.com>
2025-03-21 17:01:54 +0400

    homework_commit2

--- c_task7.c
+++ c_task7.c
@@ -1,50 +1,27 @@
 #include <stdio.h>
 
-
 int main()
-
 {
-
     int num = 0, x = 0, a = 1, tv_qanak = 0;
-
     printf("Enter your number: ");
-
     scanf("%d", &num);
     x = num;
-
     while(x>0)
-
     {
-
         x = x/10;
-
         tv_qanak++;
- 
-   }
-
+    }
     for(int i = 1; i <= tv_qanak; i++)
-
     {
-
         a = 1;
-
         for(int j = 1; j <= tv_qanak-i; j++ )
-
         {
-
            a =  10 * a;
-
         }
-
         int c = num - num%a;
-
         printf("%d, ", c/a);
-
         num  = num - c;
-
     }
 
-
     return 0;
-
 }
